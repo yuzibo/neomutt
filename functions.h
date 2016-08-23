fdb609bebd125b7d803357639e7489f81af15116
Elimar Riesebieter <riesebie@lxtec.de>
2016-08-23 20:15:31 +0200

    0008-debian-specific-dont_document_not_present_features

--- functions.h
+++ functions.h
@@ -34,7 +34,7 @@
 
 #ifdef _MAKEDOC
 #include "config.h"
-#include "doc/makedoc_defs.h"
+/* # include "doc/makedoc_defs.h" include only documented options for Debian */
 #else
 #include <stddef.h>
 #include "keymap.h"
